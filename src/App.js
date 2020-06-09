b0b6a6fba10dc445115af1f3b8ea455fb21e65fe
manu <manu.villar.vitoria@gmail.com>
2020-06-09 16:28:04 +0200

    delete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
-import {BrowserRouter}  from 'react-router-dom'; //npm i react-router react-router-dom
-
 
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Blog />
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <Blog />
+      </div>
     );
   }
 }
