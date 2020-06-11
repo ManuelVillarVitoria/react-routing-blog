7be211d229065e62eda2e2fa44800df9642df98d
manu <manu.villar.vitoria@gmail.com>
2020-06-11 15:24:05 +0200

    Using Switch to Load a Single Route and Navigating Programmatically

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -23,21 +23,23 @@ class Blog extends Component {
                                         color: "#fa293f",
                                         textDecoration: 'underline'
                                     }}
-                                    >Home</NavLink></li>
+                                    >Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</NavLink></li>
+                            }}>New Posts</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 {/*<Route path="/" exact render={ () => <h1>Home</h1> }/>
                 <Route path="/" render={ () => <h1>Home2</h1> }/>*/}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
-            </div>
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
+            </div>    
         );
     }
 }
