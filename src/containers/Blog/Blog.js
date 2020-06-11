a3c0fee262df388243ca7e932b919ca6170f4986
manu <manu.villar.vitoria@gmail.com>
2020-06-11 20:50:10 +0200

    Loading Routes Lazily

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
-import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+//import NewPost from './NewPost/NewPost';
+
+//Only we import NewPost when we use the 'const asyncNewPost' in somewhere
+//This method is fine when we want load the routes of components just we need it.
+const asyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -36,7 +43,7 @@ class Blog extends Component {
                 {/*<Route path="/" exact render={ () => <h1>Home</h1> }/>
                 <Route path="/" render={ () => <h1>Home2</h1> }/>*/}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={asyncNewPost} /> : null}
                     <Route path="/posts/" exact component={Posts} />
                     <Route render={() => <h1>Not Found</h1>} />
                      {/*<Redirect from="/" to="/posts" />*/}
